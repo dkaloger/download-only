3a6ad0b1f0a03957d469b4c6a05b294b4d7fa184
dkaloger <52400814+dkaloger@users.noreply.github.com>
2020-07-12 19:56:37 +0300

    init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+	public float _speed = 100;
+	Vector3 _velocity;
+	Vector3 _facing;
+	public float _plantDistance = 1.5f;
+	public health health;
+
+	public Dictionary<string, int> _inventory;
+	public static string[] ItemOrder = {
+		"water", "radishseeds", "cornseeds", "watermelonseeds"
+	};
+	int _currentItemIndex;
+
+	void Start() {
+		_currentItemIndex = 0;
+		_inventory = new Dictionary<string, int>();
+		_inventory.Add("water", 5);
+		_inventory.Add("radishseeds", 5);
+		_inventory.Add("cornseeds", 5);
+		_inventory.Add("watermelonseeds", 5);
+		_facing = Vector3.forward;
+	}
+
+	public Vector3 GetPlantPosition()
+    {
+		return transform.position + _facing * _plantDistance;
+    }
+
+	void FixedUpdate() {
+	
+		var rigidbody = gameObject.GetComponent<Rigidbody>();
+		if (health.stunned == false)
+		{
+			transform.Translate(_velocity * Time.fixedDeltaTime);
+			if (_velocity != Vector3.zero)
+			{
+				_facing = _velocity.normalized;
+				var model = gameObject.GetComponentInChildren<MeshRenderer>();
+				if (model)
+                {
+					model.transform.LookAt(transform.position + _facing);
+				}
+			}
+		}
+		
+	}
+
+	public void OnMove(InputValue val) {
+		var move = val.Get<Vector2>();
+		var rigidbody = gameObject.GetComponent<Rigidbody>();
+		_velocity = new Vector3(move.x, 0, move.y) * _speed;
+	}
+
+	int GetNumItem(string item) {
+		if (!_inventory.ContainsKey(item)) {
+			print("No such item "+item);
+			return 0;
+		}
+		return _inventory[item];
+	}
+
+	void CycleItem(int delta) {
+		_currentItemIndex = (_currentItemIndex + delta) % ItemOrder.Length;
+		_currentItemIndex = (_currentItemIndex + ItemOrder.Length) % ItemOrder.Length;
+
+		var item = ItemOrder[_currentItemIndex];
+		var numItem = GetNumItem(item);
+		print("Select "+item+", you have "+numItem);
+	}
+
+	public void OnNextItem(InputValue val) {
+		if (val.Get<float>() > 0)
+			CycleItem(1);
+	}
+
+	public void OnPrevItem(InputValue val) {
+		if (val.Get<float>() > 0)
+			CycleItem(-1);
+	}
+
+	public void OnScrollWheel(InputValue val) {
+		var scroll = val.Get<Vector2>();
+		print(scroll);
+	}
+
+	public void OnFire(InputValue val) {
+		if (val.Get<float>() == 0)
+			return;
+
+		var item = ItemOrder[_currentItemIndex];
+		var numItem = GetNumItem(item);
+
+		if (numItem == 0) {
+			print("Out of "+item);
+		} else {
+			--numItem;
+			print("Used "+item+", you now have "+numItem);
+			_inventory[item] = numItem;
+		}
+	}
+}
---
+++ Assets/plants/corn.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corn : MonoBehaviour
+{
+    public bool Can_attack = false;
+    public float attack_interval = 2;
+    float attack_timer;
+    public GameObject attack_missile;
+    GameObject player;
+
+    public int growthstate2startingpoint;
+
+    public int growthstate3startingpoint;
+
+    public int currentgrowthstate;
+
+    public float currentgrowth;
+
+    public float growth_coeficient;
+    public Transform tr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        currentgrowth = growth_coeficient/2;
+        attack_timer = attack_interval;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<health>() != null && Can_attack == true)
+        {
+            collision.gameObject.GetComponent<health>().HP_now -= 25;
+        }
+  
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        tr.localScale = new Vector3(currentgrowth / growth_coeficient, currentgrowth / growth_coeficient, currentgrowth / growth_coeficient);
+        tr = gameObject.GetComponent<Transform>();
+
+        if (currentgrowth < growthstate3startingpoint)
+        {
+            currentgrowth++;
+        }
+
+        if (currentgrowth > growthstate2startingpoint && currentgrowth < growthstate3startingpoint)
+        {
+            currentgrowthstate = 2;
+        }
+        if (currentgrowth + 1 > growthstate3startingpoint)
+        {
+            currentgrowthstate = 3;
+        }
+        if (currentgrowthstate == 3)
+        {
+            Can_attack = true;
+        }
+        if (Can_attack && player)
+        {
+            attack_timer -= Time.deltaTime;
+            if (attack_timer <= 0)
+            {
+                attack_timer += attack_interval;
+                Vector3 direction = (player.transform.position - transform.position);
+                direction.y = 0;
+                direction.Normalize();
+                GameObject missile = Instantiate(attack_missile, transform.position+direction, transform.rotation);
+                Physics.IgnoreCollision(GetComponent<Collider>(), missile.GetComponent<Collider>());
+                Boid boid = missile.GetComponent<Boid>();
+                boid._target = player.transform;
+            }
+        }
+    }
+}
